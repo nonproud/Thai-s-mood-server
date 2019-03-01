6d2a2be0ea70aa505791b876614e8ce56170faf3
Pawan Panphinit <panphinit@gmail.com>
2019-03-01 22:43:19 +0700

    add new module

--- database-module.js
+++ database-module.js
@@ -1,19 +1,43 @@
-var MongoClient = require('mongodb').MongoClient
+const MongoClient = require('mongodb').MongoClient
+const mail_sender = require('./mail-sender')
+const url = 'mongodb://localhost:27017'
+const dbName = 'ThaisMood'
+const assert = require('assert')
+var mydb
+const bcrypt = require('bcrypt')
+const saltRounds = 15
 
 module.exports = {
-    insertNewMember: function (req) {
-        MongoClient.connect('mongodb://localhost:27017/ThaisMood', { useNewUrlParser: true }, function (err, db) {
-            if (err) {
-                console.log(err)
-                return false
-            } else {
-                db.collection("member").find().insert(req.body, function (err, result) {
-                    if (err) throw err
-                    console.log(result)
-                    return true;
+    insertNewMember: (req, res) => {
+        MongoClient.connect(url, { useNewUrlParser: true }, (err, database) => {
+            assert.equal(null, err)
+            console.log("Ready to insert new member.")
+
+            /* Insert new member section */
+            mydb = database.db(dbName)
+            bcrypt.genSalt(saltRounds, (err, salt) => {
+                bcrypt.hash(req.body.passwd, salt, (err, hash, ) => {
+                    let insertString = {
+                        "email": req.body.email,
+                        "passwd": hash,
+                        "validate_status": req.body.v_status,
+                        "confirm_no": req.body.confirm_no
+                    }
+                    mydb.collection('member_login').insertOne(insertString, (err, result) => {
+                        if (err)
+                            res.send('Error')
+                        else {
+                            console.log(result.insertedId)
+                            console.log("Insert new member ID: " + result.insertedId)
+                            mail_sender.sendValidateMail(req.body.email, req.body.confirm_no)
+                            res.status(201).send(result.insertedId)
+                        }
+                    })
+                    database.close()
                 })
-            }            
-        })
+            })
+            /* End of insert new member */
+        });
     },
 
     connectMemberDetails: function () {
@@ -24,6 +48,8 @@ module.exports = {
         // return connectdb('record')
     }
 }
+
+
 
 
 
--- member-server.js
+++ member-server.js
@@ -2,14 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const database = require('./database-module')
 
-
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+
+
 app.post("/member", (req, res) => {
-    if(database.insertNewMember(req)){
+    if(database.insertNewMember(req, res)){
         res.status(201).json(req.body)
     } else {
         res.status(500)
@@ -48,8 +49,3 @@ app.post("/member/login", (req, res) => {
 app.listen(4553, () =>{
     console.log("Thais Mood 'Member' API was ran on PORT 4553;")
 })
-
-function writeLog(req){
-    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-    console.log(ip)
-}
