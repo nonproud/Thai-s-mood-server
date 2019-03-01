8bc57192cf4aed9930fb380992becbab4267a80c
Pawan Panphinit <panphinit@gmail.com>
2019-03-01 22:42:45 +0700

    Mail validation send

---
+++ mail-sender.js
@@ -0,0 +1,34 @@
+const nodemailer = require('nodemailer')
+const database = require('./database-module')
+/* Gmail config */
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'thaismood.validate@gmail.com', // your email
+        pass: "!zS'#s,2,8tF7}xe" // your email password
+    }
+});
+
+
+
+function getValidateMail(toEmail, confirm_code) {
+    let validate_mail = {
+        from: 'thaismood.validate@gmail.com',
+        to: toEmail,
+        subject: "รหัสยืนยันสำหรับสมัครแอปพลิเคชั่น Thai's Mood",
+        html: "<h1>" + confirm_code + "</h1>"
+    }
+    return validate_mail
+}
+
+module.exports = {
+    sendValidateMail: function (email, confirm_code) {
+        transporter.sendMail(getValidateMail(email, confirm_code), function (err, info) {
+            if (err)
+                console.log(err)
+            else
+                console.log(info);
+        });
+        // console.log("Email: " + email + " | code: " + confirm_code)
+    }
+}
