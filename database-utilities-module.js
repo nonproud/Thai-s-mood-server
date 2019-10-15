aa4841051e76c75e44fb0d9231b2ad57a98afc57
Pawan Panphinit <panphinit@gmail.com>
2019-10-15 21:31:00 +0700

    Add utility module for get email password for DB

--- database-utilities-module.js
+++ database-utilities-module.js
@@ -18,7 +18,7 @@ function getEmailPassword(){
     console.log(sql)
     pool.getConnection().then(conn => {
         conn.query(sql).then(result => {
-            console.log("mailpass: " + result[0])
+            console.log("mailpass: " + result)
             password =  result[0]
             conn.end()
             console.log("mailpass: " + password)
